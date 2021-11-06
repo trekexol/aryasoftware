d1a8b758047a68244a3cee7d02a481175dbceefb
sirdax99 <sirdax99@gmail.com>
2021-11-06 14:44:33 -0400

    agregando de nuevo gsa en data base

--- config/database.php
+++ config/database.php
@@ -106,6 +106,27 @@ return [
             ]) : [],
         ],
 
+        
+        'gsa' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('GSA_HOST', '127.0.0.1'),
+            'port' => env('GSA_PORT', '3306'),
+            'database' => env('GSA_DATABASE', 'forge'),
+            'username' => env('GSA_USERNAME', 'forge'),
+            'password' => env('GSA_PASSWORD', ''),
+            'unix_socket' => env('GSA_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'aryanorthdelivery' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
