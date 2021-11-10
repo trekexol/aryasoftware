17defea6f9506bc0402f732b4b447bfc29e1c135
trekexol <cefreitas.16@gmail.com>
2021-11-10 08:40:52 -0400

    dsf

--- resources/views/admin/reports/selectclient.blade.php
+++ resources/views/admin/reports/selectclient.blade.php
@@ -29,7 +29,7 @@
                 <tr> 
                     <th></th>
                    
-                    <th>Nombre</th>
+                    <th>Nombre h</th>
                     <th>Cedula o Rif</th>
                     <th>Dirección</th>
                     <th>Ciudad</th>
